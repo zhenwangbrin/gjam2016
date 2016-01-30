4d8f99d20b7e336f81bddd3e51b6f9f23c70b695
zhenwangbrin <zhenwang@Zhens-iMac.local>
2016-01-30 15:37:11 -0700

    making add player work

--- public/remote/client.js
+++ public/remote/client.js
@@ -1,19 +1,25 @@
-// reference to watch for squenceChanges, meaning the start of a new round.
-var squenceRef = new Firebase("https://gjam2016.firebaseio.com/sequence");
+// reference my player
+var mePlayerRef = playersRef[parseInt(getParameterByName('player'))];
 
-squenceRef.on('value', function (dataSnapshot) {
+var playerSequence = [];
+var serverSequence = [];
+var enteredSequence = [];
+
+if (!Date.now) {
+    Date.now = function () {
+        return new Date().getTime();
+    }
+}
+
+sequenceRef.on('value', function (dataSnapshot) {
     // code to handle new value.
+    document.getElementById("enteredSequence").innerHTML = "";
+    randomizeOptions(dataSnapshot.val());
 });
-
-// reference to watch for rain position change to update the rain position.
-var rainRef = new Firebase("https://gjam2016.firebaseio.com/rain");
 
 rainRef.on('value', function (dataSnapshot) {
-    // code to handle new value.
-});
 
-// reference to players to read other player changes and update own player.
-var playersRef = new Firebase("https://gjam2016.firebaseio.com/players");
+});
 
 playersRef.on('child_added', function (childSnapshot, prevChildKey) {
     // code to handle new player joining.
@@ -27,17 +33,61 @@ playersRef.on('child_changed', function (childSnapshot, prevChildKey) {
     // code to display changes to players.
 });
 
-// function to generate the randomized player keypads base off the current sequence.
-function randomizeOptions() {
+//function to randomize array
+function shuffle(array) {
+    var counter = array.length;
 
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        var index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        var temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+// function to generate the randomized player keypads base off the current sequence.
+function randomizeOptions(seq) {
+    serverSequence = seq;
+    playerSequence = serverSequence.slice(0);
+    playerSequence.push(Math.floor(Math.random() * serverSequence.length));
+    playerSequence.push(Math.floor(Math.random() * serverSequence.length));
+    playerSequence = shuffle(playerSequence);
 }
 
 // function to handle incoming keypad strokes to test against the current sequence.
-function keyStroke(strokeId) {
-
+function keyStroke(keyIndex) {
+    var currentKey = enteredSequence.length
+    if (playerSequence[keyIndex] === serverSequence[currentKey]) {
+        enteredSequence.push(playerSequence[currentKey]);
+        document.getElementById("enteredSequence").innerHTML = enteredSequence.join(' ');
+        checkKeyStroke();
+    } else {
+        enteredSequence.removeAll();
+        document.getElementById("enteredSequence").innerHTML = 'X';
+    }
 }
 
 // function to update the client time stamp if a sequence was correctly entered.
-function strokeSucceed() {
+function checkKeyStroke() {
+    if (enteredSequence.length === serverSequence.length) {
+        mePlayerRef.completed = true;
+        mePlayerRef.finishTime = Date.now();
+    }
+}
 
+// function to parse out player id or index
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
---
+++ public/shared/main.js
@@ -0,0 +1,47 @@
+// reference to watch for sequenceChanges, meaning the start of a new round.
+var sequenceRef = new Firebase("https://gjam2016.firebaseio.com/seq");
+
+// reference to watch for rain position change to update the rain position.
+var rainRef = new Firebase("https://gjam2016.firebaseio.com/rain");
+
+// reference to players to read other player changes and update own player.
+var playersRef = new Firebase("https://gjam2016.firebaseio.com/players");
+
+var mePlayerRef = {};
+var currentPlayerKey = -1;
+
+// when value comes for players
+playersRef.on("value", function (snapshot) {
+    updatePlayers(snapshot);
+})
+
+// function to update game players
+function updatePlayers(snapshot) {
+    var checkIndex = 0;
+    mePlayerRef = snapshot.val()[checkIndex];
+    while (mePlayerRef.name !== undefined) {
+        checkIndex++;
+        if (checkIndex >= snapshot.val().length) {
+            break;
+        }
+        mePlayerRef = snapshot.val()[checkIndex];
+    }
+
+    if (mePlayerRef.name !== undefined) {
+        document.getElementById("infoDisplay").innerHTML = "Game Full! game stat: " + playersRef.val();
+        currentPlayerKey = -1;
+        return;
+    }
+
+    currentPlayerKey = checkIndex;
+}
+
+// function to add new player
+function addPlayer() {
+    var playerTag = document.getElementById("playerTag").value;
+    playersRef.child(currentPlayerKey).update({
+        name: playerTag
+    });
+    updatePlayers = function () {};
+    location.href = '/remote/client.html?player=' + currentPlayerKey;
+}
